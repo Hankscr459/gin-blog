14c27f99ae935a42913406ae65c7d1a3fa9ccce7
hank <akysn564@gmail.com>
2022-11-02 11:19:45 +0800

    style: change fileName

--- plugins/dto/common.go
+++ plugins/dto/common.go
@@ -2,6 +2,7 @@ package dto
 
 import (
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -10,4 +11,11 @@ type Cliam struct {
 	ID    primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	Name  string             `json:"name"`
 	jwt.StandardClaims
+}
+
+type PageParamsInput struct {
+	Ctx        *gin.Context `json:"ctx" bson:"Ctx"`
+	SearchType []string     `json:"search_type"`
+	// "asc" or "desc" 1, -1 exp: price,-1
+	DeSelect []string `json:"de_select"`
 }
